7aac39ff6e7c950e0af8ba935786bf8a256bb46c
Hendrik Kunert <hendrik.kunert@gmail.com>
2024-01-19 16:46:22 +0100

    Add time representation in plots

--- src/bin/gui.rs
+++ src/bin/gui.rs
@@ -20,7 +20,15 @@ use iced::{
     },
     Application, Command, Element, Event, Font, Length, Settings, Size, Subscription, Theme,
 };
-use plotters::{coord::types::RangedCoordi64, prelude::*};
+use plotters::{
+    coord::{
+        ranged1d::{
+            DefaultFormatting, DefaultValueFormatOption, NoDefaultFormatting, ValueFormatter,
+        },
+        types::RangedCoordi64,
+    },
+    prelude::*,
+};
 use plotters::{
     coord::{
         types::{RangedCoordf32, RangedCoordusize},
@@ -46,11 +54,13 @@ fn main() {
 enum Message {
     ComputeFrequencyResponse,
     ImpulseRespone(ImpulseResponseMessage),
+    New(TimeSeriesMessageNew),
     Back,
 }
 
 struct State {
     chart: ImpulseResponseChart,
+    new_chart: TimeseriesChartNew,
     frequency_response: Option<()>,
 }
 
@@ -81,10 +91,16 @@ impl Application for State {
             Some(AmplitudeUnit::PercentFullScale),
         );
 
+        let new_chart = TimeseriesChartNew::new(
+            WindowBuilder::new(Window::Hann, Window::Hann, 44100).build().into_iter(),
+            TimeSeriesUnit::Samples
+        );
+
         let chart = ImpulseResponseChart::new(chart);
         (
             Self {
                 chart,
+                new_chart,
                 frequency_response: None,
             },
             Command::none(), //Command::batch([
@@ -126,7 +142,7 @@ impl Application for State {
                 let frequency_response: Vec<_> = windowed_ir
                     .iter()
                     .enumerate()
-                    .map(|(n, s)| (n as i64 , s.re))
+                    .map(|(n, s)| (n as i64, s.re))
                     .collect();
                 self.chart = ImpulseResponseChart::new(TimeseriesChart::new(
                     "FR".to_string(),
@@ -141,6 +157,7 @@ impl Application for State {
             }
             Message::Back => self.frequency_response = None,
             Message::ImpulseRespone(msg) => self.chart.update_msg(msg),
+            Message::New(msg) => self.new_chart.update_msg(msg),
         }
 
         Command::none()
@@ -153,7 +170,8 @@ impl Application for State {
             widget::button("FR").on_press(Message::ComputeFrequencyResponse)
         };
 
-        widget::column!(btn, self.chart.view().map(Message::ImpulseRespone)).into()
+        //widget::column!(btn, self.chart.view().map(Message::ImpulseRespone)).into()
+        widget::column!(btn, self.new_chart.view().map(Message::New)).into()
     }
 
     fn subscription(&self) -> Subscription<Self::Message> {
@@ -700,9 +718,11 @@ impl TimeseriesChart {
 
         // FIXME: precompute on amplitude change
         self.processed_data = match &self.amplitude_unit {
-            Some(AmplitudeUnit::PercentFullScale) => {
-                self.data.iter().map(|(n, s)| (*n, *s / max * 100f32)).collect()
-            }
+            Some(AmplitudeUnit::PercentFullScale) => self
+                .data
+                .iter()
+                .map(|(n, s)| (*n, *s / max * 100f32))
+                .collect(),
             Some(AmplitudeUnit::DezibelFullScale) => self
                 .data
                 .iter()
@@ -809,5 +829,177 @@ impl Chart<TimeSeriesMessage> for TimeseriesChart {
             }
         }
         (event::Status::Ignored, None)
+    }
+}
+
+enum TimeSeriesRange {
+    Samples(RangedCoordusize),
+    Time(usize, RangedCoordusize),
+}
+
+impl ValueFormatter<usize> for TimeSeriesRange {
+    fn format_ext(&self, value: &usize) -> String {
+        match self {
+            TimeSeriesRange::Samples(_) => format!("{}", value),
+            TimeSeriesRange::Time(sample_rate, _) => {
+                format!("{}", *value as f32 / *sample_rate as f32)
+            }
+        }
+    }
+}
+
+impl Ranged for TimeSeriesRange {
+    type FormatOption = NoDefaultFormatting;
+
+    type ValueType = usize;
+
+    fn map(&self, value: &Self::ValueType, limit: (i32, i32)) -> i32 {
+        match self {
+            TimeSeriesRange::Samples(ranged) => ranged.map(value, limit),
+            TimeSeriesRange::Time(_sample_rate, ranged) => ranged.map(value, limit),
+        }
+    }
+
+    fn key_points<Hint: plotters::coord::ranged1d::KeyPointHint>(
+        &self,
+        hint: Hint,
+    ) -> Vec<Self::ValueType> {
+        match self {
+            TimeSeriesRange::Samples(ranged) => ranged.key_points(hint),
+            TimeSeriesRange::Time(_, ranged) => ranged.key_points(hint),
+        }
+    }
+
+    fn range(&self) -> Range<Self::ValueType> {
+        match self {
+            TimeSeriesRange::Samples(ranged) => ranged.range(),
+            TimeSeriesRange::Time(_, ranged) => ranged.range(),
+        }
+    }
+
+    fn axis_pixel_range(&self, limit: (i32, i32)) -> Range<i32> {
+        if limit.0 < limit.1 {
+            limit.0..limit.1
+        } else {
+            limit.1..limit.0
+        }
+    }
+}
+
+pub struct TimeseriesChartNew {
+    cache: Cache,
+    data: Vec<f32>,
+    time_unit: TimeSeriesUnit,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum TimeSeriesUnit {
+    Samples,
+    Time,
+}
+
+impl TimeSeriesUnit {
+    const ALL: [Self; 2] = [TimeSeriesUnit::Samples, TimeSeriesUnit::Time];
+}
+
+impl std::fmt::Display for TimeSeriesUnit {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                TimeSeriesUnit::Samples => "Samples",
+                TimeSeriesUnit::Time => "Time",
+            }
+        )
+    }
+}
+
+#[derive(Debug, Clone)]
+enum TimeSeriesMessageNew {
+    TimeUnitChanged(TimeSeriesUnit),
+}
+
+impl TimeseriesChartNew {
+    fn new(data: impl Iterator<Item = f32>, time_unit: TimeSeriesUnit) -> Self {
+        let data = data.collect();
+        Self {
+            data,
+            time_unit,
+            cache: Cache::new(),
+        }
+    }
+
+    fn view(&self) -> Element<TimeSeriesMessageNew> {
+        let header = widget::row!(widget::pick_list(
+            &TimeSeriesUnit::ALL[..],
+            Some(self.time_unit.clone()),
+            TimeSeriesMessageNew::TimeUnitChanged
+        ));
+        Container::new(
+            Column::new()
+                .width(Length::Fill)
+                .height(Length::Fill)
+                .spacing(5)
+                .push(header)
+                .push(ChartWidget::new(self).height(Length::Fill)),
+        )
+        .width(Length::Fill)
+        .height(Length::Fill)
+        .align_x(Horizontal::Center)
+        .align_y(Vertical::Center)
+        .into()
+    }
+
+    fn update_msg(&mut self, msg: TimeSeriesMessageNew) {
+        match msg {
+            TimeSeriesMessageNew::TimeUnitChanged(u) => {
+                self.time_unit = u;
+                self.cache.clear();
+            }
+        }
+    }
+}
+
+impl Chart<TimeSeriesMessageNew> for TimeseriesChartNew {
+    type State = ();
+
+    #[inline]
+    fn draw<R: Renderer, F: Fn(&mut Frame)>(
+        &self,
+        renderer: &R,
+        bounds: Size,
+        draw_fn: F,
+    ) -> Geometry {
+        renderer.draw_cache(&self.cache, bounds, draw_fn)
+    }
+
+    fn build_chart<DB: DrawingBackend>(&self, _state: &Self::State, mut builder: ChartBuilder<DB>) {
+        use plotters::prelude::*;
+
+        let x_range = match self.time_unit {
+            TimeSeriesUnit::Samples => TimeSeriesRange::Samples((0..self.data.len()).into()),
+            TimeSeriesUnit::Time => TimeSeriesRange::Time(44100, (0..self.data.len()).into()),
+        };
+
+        let min = self.data.iter().fold(f32::INFINITY, |a, b| a.min(*b));
+        let max = self.data.iter().fold(f32::NEG_INFINITY, |a, b| a.max(*b));
+        let mut chart = builder
+            .margin(5)
+            .x_label_area_size(30)
+            .y_label_area_size(30)
+            .build_cartesian_2d(x_range, min..max)
+            .unwrap();
+
+        chart
+            .draw_series(LineSeries::new(self.data.iter().cloned().enumerate(), &RED))
+            .unwrap();
+
+        chart
+            .configure_mesh()
+            .disable_mesh()
+            //.disable_axes()
+            .draw()
+            .unwrap();
     }
 }
