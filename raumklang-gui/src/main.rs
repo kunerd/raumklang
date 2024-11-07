af4f40c406bfeec5b18aaef77c43ab4d4f125302
Hendrik Kunert <hendrik.kunert@gmail.com>
2024-11-07 21:10:30 +0100

    Refactor signal list entries

--- raumklang-gui/src/main.rs
+++ raumklang-gui/src/main.rs
@@ -3,10 +3,7 @@ mod widgets;
 
 use std::path::Path;
 
-use iced::{
-    widget::{column, text},
-    Element, Font, Task,
-};
+use iced::{Element, Font, Task};
 use iced_aw::Tabs;
 
 use tabs::{signals::WavLoadError, Tab};
@@ -24,21 +21,6 @@ struct State {
     signals_tab: tabs::Signals,
     impulse_response_tab: tabs::ImpulseResponse,
 }
-
-//impl State {
-//    fn new() -> (Self, Task<Message>) {
-//        (
-//            Self {
-//                active_tab: TabId::Signals,
-//                signals_tab: tabs::Signals::
-//                loopback_signal: None,
-//                measurement_signal: None,
-//                chart: None,
-//            },
-//            Task::none(),
-//        )
-//    }
-//}
 
 #[derive(Debug, Clone)]
 enum Message {
@@ -78,70 +60,9 @@ impl State {
                     .map(Message::ImpulseResponse),
             )
             .set_active_tab(&self.active_tab)
-            //.tab_bar_style(style_from_index(theme))
-            //.icon_font(ICON)
             .tab_bar_position(iced_aw::TabBarPosition::Top);
 
         tabs.into()
-        //let side_menu = {
-        //    let loopback_entry = {
-        //        let header = text("Loopback");
-        //        let btn = if let Some(signal) = &self.loopback_signal {
-        //            button(signal.view()).on_press(Message::LoopbackSignalSelected)
-        //        } else {
-        //            button(text("load ...".to_string())).on_press(Message::LoadLoopbackSignal)
-        //        }
-        //        .style(button::secondary);
-
-        //        column!(header, btn).width(Length::Fill).spacing(5)
-        //    };
-
-        //    let measurement_entry = {
-        //        let header = text("Measurements");
-        //        let btn = if let Some(signal) = &self.measurement_signal {
-        //            button(signal.view()).on_press(Message::MeasurementSignalSelected)
-        //        } else {
-        //            button(text("load ...".to_string())).on_press(Message::LoadMeasurementSignal)
-        //        }
-        //        .style(button::secondary);
-
-        //        column!(header, btn).width(Length::Fill).spacing(5)
-        //    };
-
-        //    container(column!(loopback_entry, measurement_entry).spacing(10))
-        //        .padding(5)
-        //        .width(Length::FillPortion(1))
-        //};
-
-        //let right_container = Tabs::new(Message::TabSelected)
-        //    //.tab_icon_position(iced_aw::tabs::Position::Bottom)
-        //    .on_close(Message::TabClosed)
-        //    .push(
-        //        TabId::Measurements,
-        //        iced_aw::TabLabel::Text("Measurements".to_string()),
-        //        {
-        //            if let Some(chart) = &self.chart {
-        //                container(chart.view().map(Message::TimeSeriesChart))
-        //                    .width(Length::FillPortion(5))
-        //            } else {
-        //                container(text("Not implemented.".to_string()))
-        //            }
-        //        },
-        //    )
-        //    .push(
-        //        TabId::RoomResponse,
-        //        iced_aw::TabLabel::Text("Room impulse response".to_string()),
-        //        text("Not implemented"),
-        //    )
-        //    .set_active_tab(&self.active_tab)
-        //    //.tab_bar_style(style_from_index(theme))
-        //    //.icon_font(ICON)
-        //    .tab_bar_position(iced_aw::TabBarPosition::Top);
-
-        //let right_container = right_container.width(Length::FillPortion(4));
-
-        //let content = row!(side_menu, right_container);
-        //content.into()
     }
 }
 
@@ -153,18 +74,6 @@ struct Signal {
 }
 
 impl Signal {
-    pub fn view(&self) -> Element<tabs::signals::SignalsMessage> {
-        let samples = self.data.len();
-        let sample_rate = self.sample_rate as f32;
-        column!(
-            text(&self.name),
-            text(format!("Samples: {}", samples)),
-            text(format!("Duration: {} s", samples as f32 / sample_rate)),
-        )
-        .padding(2)
-        .into()
-    }
-
     pub fn from_file(path: impl AsRef<Path>) -> Result<Self, WavLoadError> {
         let name = path
             .as_ref()
