d459185baab9772e1baf5c9f29a0d50f70f4fd4c
Hendrik Kunert <hendrik.kunert@gmail.com>
2025-03-24 22:09:32 +0100

    Remove dead code

--- raumklang-gui/src/data/mod.rs
+++ raumklang-gui/src/data/mod.rs
@@ -1,10 +1,7 @@
 pub mod measurement;
 pub mod project;
 mod recent_projects;
-// mod store;
+
 pub use measurement::Measurement;
 pub use project::Project;
 pub use recent_projects::RecentProjects;
-// pub use store::{Id, MeasurementState, Store};
-
-// pub type MeasurementId = Id<Measurement>;
