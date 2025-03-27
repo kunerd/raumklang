d101fe9912d3dd2596ad962444fb41ddeb711d7a
Hendrik Kunert <hendrik.kunert@gmail.com>
2025-03-27 20:35:17 +0100

    Add window for frequency response computation

--- raumklang-gui/src/data.rs
+++ raumklang-gui/src/data.rs
@@ -3,11 +3,13 @@ pub mod impulse_response;
 pub mod measurement;
 pub mod project;
 mod recent_projects;
+pub mod window;
 
 pub use impulse_response::ImpulseResponse;
 pub use measurement::Measurement;
 pub use project::Project;
 pub use recent_projects::RecentProjects;
+pub use window::Window;
 
 use std::{io, sync::Arc};
 
