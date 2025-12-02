935cede56fce2eb36f617a24b11b48b12651adfd
Hendrik Kunert <hendrik.kunert@gmail.com>
2025-12-02 10:46:40 +0100

    Simplify spectral decay state update

--- raumklang-gui/src/data/spectral_decay.rs
+++ raumklang-gui/src/data/spectral_decay.rs
@@ -7,11 +7,6 @@ use rustfft::{num_complex::Complex, FftPlanner};
 
 use crate::data::{smooth_fractional_octave, SampleRate, Samples};
 
-#[derive(Debug, Clone)]
-pub enum Event {
-    ComputingStarted,
-}
-
 pub struct Preferences {
     shift: Duration,
     left_window_width: Duration,
@@ -32,71 +27,68 @@ impl SpectralDecay {
     }
 }
 
-pub(crate) fn compute(
+pub(crate) async fn compute(
     mut ir: raumklang_core::ImpulseResponse,
     preferences: Preferences,
-) -> impl Sipper<SpectralDecay, Event> {
-    sipper(move |mut output| async move {
-        let sample_rate = SampleRate::from(ir.sample_rate);
-        output.send(Event::ComputingStarted).await;
+) -> SpectralDecay {
+    let sample_rate = SampleRate::from(ir.sample_rate);
 
-        let shift: usize = Samples::from_duration(preferences.shift, sample_rate).into();
-        let left_width = Samples::from_duration(preferences.left_window_width, sample_rate);
-        let right_width = Samples::from_duration(preferences.right_window_width, sample_rate);
+    let shift: usize = Samples::from_duration(preferences.shift, sample_rate).into();
+    let left_width = Samples::from_duration(preferences.left_window_width, sample_rate);
+    let right_width = Samples::from_duration(preferences.right_window_width, sample_rate);
 
-        let window = WindowBuilder::new(
-            Window::Hann,
-            left_width.into(),
-            Window::Tukey(0.25),
-            right_width.into(),
-        );
-        let window = window.build();
+    let window = WindowBuilder::new(
+        Window::Hann,
+        left_width.into(),
+        Window::Tukey(0.25),
+        right_width.into(),
+    );
+    let window = window.build();
 
-        ir.data.rotate_right(left_width.into());
+    ir.data.rotate_right(left_width.into());
 
-        let mut start = 0;
-        let window_size = usize::from(left_width + right_width);
+    let mut start = 0;
+    let window_size = usize::from(left_width + right_width);
 
-        tokio::task::spawn_blocking(move || {
-            let mut frequency_responses = Vec::with_capacity(window_size / shift);
+    tokio::task::spawn_blocking(move || {
+        let mut frequency_responses = Vec::with_capacity(window_size / shift);
 
-            let mut planner = FftPlanner::<f32>::new();
-            let fft = planner.plan_fft_forward(window_size);
+        let mut planner = FftPlanner::<f32>::new();
+        let fft = planner.plan_fft_forward(window_size);
 
-            while start < window_size.into() {
-                let ir_slice = &ir.data[start..start + window_size];
-                let mut windowed_impulse_response: Vec<_> = ir_slice
-                    .iter()
-                    .copied()
-                    .enumerate()
-                    .map(|(i, s)| s * window[i])
-                    .collect();
+        while start < window_size.into() {
+            let ir_slice = &ir.data[start..start + window_size];
+            let mut windowed_impulse_response: Vec<_> = ir_slice
+                .iter()
+                .copied()
+                .enumerate()
+                .map(|(i, s)| s * window[i])
+                .collect();
 
-                fft.process(&mut windowed_impulse_response);
+            fft.process(&mut windowed_impulse_response);
 
-                let data_len = windowed_impulse_response.len() / 2 - 1;
-                let data: Vec<_> = windowed_impulse_response
-                    .into_iter()
-                    .take(data_len)
-                    .map(Complex::norm)
-                    .collect();
+            let data_len = windowed_impulse_response.len() / 2 - 1;
+            let data: Vec<_> = windowed_impulse_response
+                .into_iter()
+                .take(data_len)
+                .map(Complex::norm)
+                .collect();
 
-                let data = smooth_fractional_octave(&data, preferences.smoothing_fraction);
+            let data = smooth_fractional_octave(&data, preferences.smoothing_fraction);
 
-                let sample_rate = ir.sample_rate;
-                frequency_responses.push(super::FrequencyResponse {
-                    sample_rate,
-                    data: Arc::new(data),
-                });
+            let sample_rate = ir.sample_rate;
+            frequency_responses.push(super::FrequencyResponse {
+                sample_rate,
+                data: Arc::new(data),
+            });
 
-                start += shift;
-            }
+            start += shift;
+        }
 
-            SpectralDecay(frequency_responses)
-        })
-        .await
-        .unwrap()
+        SpectralDecay(frequency_responses)
     })
+    .await
+    .unwrap()
 }
 
 impl fmt::Debug for SpectralDecay {
